18c950bb758d77e11a5186da3ac7ae129d138cad
Shaine123 <shaineberdida@gmail.com>
2026-05-27 21:27:53 +0800

    added the functions and additional styles

--- script.js
+++ script.js
@@ -2,11 +2,38 @@ const form = document.getElementById("form");
 const userName = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const confirmPassword = document.getElementById("confirmPassword");
+const confirmPassword = document.getElementById("confirmpassword");
 
 form.addEventListener("submit", submitForm);
 
 function submitForm(e) {
   e.preventDefault();
-  console.log("test");
+
+  const validateForm = checkFormInput([
+    username,
+    email,
+    password,
+    confirmPassword,
+  ]);
+}
+
+function checkFormInput(inputArray) {
+  let isValid = true;
+
+  inputArray.forEach((input) => {
+    if (input.value.trim() == "") {
+      showError(input, `${formFormatter(input)} is empty`);
+    }
+  });
+
+  return isValid;
+}
+
+function showError(input, message) {
+  console.log(input);
+  console.log(message);
+}
+
+function formFormatter(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
